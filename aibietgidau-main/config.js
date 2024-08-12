d1bac8b7fc6be7ea05f133ba16e4c54c742b77f1
caffeine2711 <85494515+caffeine2711@users.noreply.github.com>
2024-08-12 22:46:30 +0700

    Add files via upload

---
+++ aibietgidau-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bạn à! -____-',
+    introDesc: `Cổ nhân có câu:
+    zảo shang hảo tung của
+    xiên zai wuo zhou Bing Chilling
+    wuo han xi khoan Bing Chilling`,
+    btnIntro: '😒😒😒',
+    title: 'Mua đầy đủ rồi xin địa chỉ để ship thì không cho -_-',
+    desc: 'Làm 3 thằng hôm qua uống bia ăn bim bim',
+    btnYes: 'Nhấn vào đây để cho địa chỉ',
+    btnNo: 'Để suy nghĩ lại đã',
+    question:'Cho rồi thì nhập địa chỉ vào đây nha ',
+    btnReply: 'Gửi :D',
+    reply: 'Để gửi địa chỉ nhà qua facebook cho ha :D',
+    mess: 'hehehe, được luôn 😎',
+    messDesc: 'Tối nay 7h có mặt trước cửa nhà nhá',
+    btnAccept: 'eheheheheh',
+    messLink: 'https://www.facebook.com/3107.lilraven'
+}
